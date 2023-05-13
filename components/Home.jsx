36464d4aa2e4604e6e55d39e19c8cf5159df678d
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2023-05-13 20:31:24 +0500

    Updated pic

--- components/Home.jsx
+++ components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <img
           class=" lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
           alt="hero"
-          src="profile-favicon.svg"
+          src="circle-dp.png"
         />
         <div class="text-center lg:w-2/3 w-full">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white ">
